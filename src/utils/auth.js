cb0c93a95e80ee4c1f88887c00d00f531974308a
VMR21 <reddymodith@gmail.com>
2025-04-09 22:01:21 +0530

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
 export const checkCredentials = (email, password) => {
   // Adjust boss credentials here:
-  return email === "boss@example.com" && password === "supersecure123";
+  return email === "azisai" && password === "07219";
 };
