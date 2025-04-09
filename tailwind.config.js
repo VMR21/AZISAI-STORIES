add0d3ceafa5e22fc7e71e75ed747aaabdcbfbf7
VMR21 <reddymodith@gmail.com>
2025-04-10 01:19:57 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 extend: {
   animation: {
     waveMotion: "wave 10s linear infinite",
-    stars: "stars 100s linear infinite",
   },
   keyframes: {
     wave: {
       "0%": { transform: "translateX(0)" },
       "100%": { transform: "translateX(-50%)" },
     },
-    stars: {
-      "0%": { backgroundPosition: "0 0" },
-      "100%": { backgroundPosition: "1000px 1000px" },
-    },
   },
-},
+}
