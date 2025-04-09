347dcfdf583c762f1be3b16686d98f62b173d869
VMR21 <reddymodith@gmail.com>
2025-04-10 02:59:57 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
-extend: {
-  animation: {
-    waveMotion: "wave 10s linear infinite",
-  },
-  keyframes: {
-    wave: {
-      "0%": { transform: "translateX(0)" },
-      "100%": { transform: "translateX(-50%)" },
+theme: {
+  extend: {
+    animation: {
+      pulseGlow: "pulse-glow 4s infinite",
+      waveMotion: "wave 15s linear infinite",
+    },
+    keyframes: {
+      "pulse-glow": {
+        "0%, 100%": { opacity: "0.6" },
+        "50%": { opacity: "1" },
+      },
+      wave: {
+        "0%": { transform: "translateX(0)" },
+        "100%": { transform: "translateX(-50%)" },
+      },
     },
   },
-}
+},
