93467ed468863c84a16e95b97886c36700ac0abf
VMR21 <reddymodith@gmail.com>
2025-04-10 00:19:30 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+module.exports = {
+  // ...
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        float: 'float 15s linear infinite',
+        'spin-slow': 'spin 20s linear infinite',
+      },
+      keyframes: {
+        float: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-100vh)' },
+        },
+      },
+    },
   },
-  plugins: [],
-}
+};
