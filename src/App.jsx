9431f4d1cf85d16aea3fed706da1c11295879c2d
VMR21 <reddymodith@gmail.com>
2025-04-09 23:05:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import NewPost from "./pages/NewPost";
-import ViewPost from "./pages/ViewPost";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/new" element={<NewPost />} />
-        <Route path="/post/:id" element={<ViewPost />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
